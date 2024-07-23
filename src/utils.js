15d37b9258be5f403c6745f1d23799fe74f0b348
Chanel Morgan <chanelmorgan05@icloud.com>
2024-07-23 12:20:55 +0100

    Fixing issues

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,45 @@
-export function displayDialogue(text, onDisplayEnd) { 
-    const dialogueUI = document.getElementById("textbox-container"); 
-    const dialogue = document.getElementById("dialogue"); 
+export function displayDialogue(text, onDisplayEnd) {
+  const dialogueUI = document.getElementById("textbox-container");
+  const dialogue = document.getElementById("dialogue");
 
-    dialogueUI.style.display = "block";
+  dialogueUI.style.display = "block";
+  let index = 0;
+  let currentText = "";
+  const intervalRef = setInterval(() => {
+    if (index < text.length) {
+      currentText += text[index];
+      dialogue.innerHTML = currentText;
+      index++;
+      return;
+    }
 
-    let index = 0; 
-    let currentText = ""; 
-    const intervalRef = setInterval(() => {
-        if(index < text.length){ 
-            currentText += text[index]; 
-            dialogueUI.innerHTML = currentText; 
-            index++; 
-            return; 
-        } 
-        clearInterval(intervalRef);
-        
-    }, 5);
+    clearInterval(intervalRef);
+  }, 1);
 
+  const closeBtn = document.getElementById("close");
 
-    const closeBtn = document.getElementById("close"); 
-    
-    function onCloseBtnClick() { 
+  function onCloseBtnClick() {
+    onDisplayEnd();
+    dialogueUI.style.display = "none";
+    dialogue.innerHTML = "";
+    clearInterval(intervalRef);
+    closeBtn.removeEventListener("click", onCloseBtnClick);
+  }
 
-        onDisplayEnd(); 
-        dialogueUI.style.display = "none";
-        dialogueUI.innerHTML = ""; 
-        clearInterval(intervalRef);
-        closeBtn.removeEventListener("click", onCloseBtnClick); 
-    } 
+  closeBtn.addEventListener("click", onCloseBtnClick);
 
-    closeBtn.addEventListener("click", onCloseBtnClick);
+  addEventListener("keypress", (key) => {
+    if (key.code === "Enter") {
+      closeBtn.click();
+    }
+  });
+}
 
-    
-
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height();
+  if (resizeFactor < 1) {
+    k.camScale(k.vec2(1));
+  } else {
+    k.camScale(k.vec2(1.5));
+  }
 }
