8985dd07bf0594e8eff4236575e2583e05caec80
Chanel Morgan <chanelmorgan05@icloud.com>
2024-07-23 11:49:53 +0100

    Writing logoc to display dialogue

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+export function displayDialogue(text, onDisplayEnd) { 
+    const dialogueUI = document.getElementById("textbox-container"); 
+    const dialogue = document.getElementById("dialogue"); 
+
+    dialogueUI.style.display = "block";
+
+    let index = 0; 
+    let currentText = ""; 
+    const intervalRef = setInterval(() => {
+        if(index < text.length){ 
+            currentText += text[index]; 
+            dialogueUI.innerHTML = currentText; 
+            index++; 
+            return; 
+        } 
+        clearInterval(intervalRef);
+        
+    }, 5);
+
+
+    const closeBtn = document.getElementById("close"); 
+    
+    function onCloseBtnClick() { 
+
+        onDisplayEnd(); 
+        dialogueUI.style.display = "none";
+        dialogueUI.innerHTML = ""; 
+        clearInterval(intervalRef);
+        closeBtn.removeEventListener("click", onCloseBtnClick); 
+    } 
+
+    closeBtn.addEventListener("click", onCloseBtnClick);
+
+    
+
+}
