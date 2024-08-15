9a73ebbdf645082de4d1bdf3a86aabfaa6c71897
Chanel Morgan <chanelmorgan05@icloud.com>
2024-08-15 14:51:35 +0100

    REDIOING

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/Portfolio2DGame/', // Ensure this matches your repository name
-  build: {
-    outDir: 'dist', // Ensure this matches the publish_dir in your GitHub Actions
-    minify: 'terser',
-  },
-});
